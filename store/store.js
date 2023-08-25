24b172366e971a7bffc308930b9e12a0ad5a6ad9
Amlan2002 <iamamlan2002@gmail.com>
2023-08-25 12:24:04 +0530

    redux persist turn off

--- store/store.js
+++ store/store.js
@@ -1,40 +1,45 @@
 import {configureStore} from '@reduxjs/toolkit';
 import {userSlice} from './userSlice';
 import {postSlice} from './postSlice';
-
-import {combineReducers} from '@reduxjs/toolkit';
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import {reelSlice} from './reelSlice';
 
-const persistConfig = {
-  key: 'root',
-  storage: AsyncStorage,
-};
+// import {combineReducers} from '@reduxjs/toolkit';
+// import {
+//   persistStore,
+//   persistReducer,
+//   FLUSH,
+//   REHYDRATE,
+//   PAUSE,
+//   PERSIST,
+//   PURGE,
+//   REGISTER,
+// } from 'redux-persist';
+// import AsyncStorage from '@react-native-async-storage/async-storage';
 
-let rootReducer = combineReducers({
-  user: userSlice.reducer,
-  post: postSlice.reducer,
-  reel: reelSlice.reducer,
-});
+// const persistConfig = {
+//   key: 'root',
+//   storage: AsyncStorage,
+// };
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
+// let rootReducer = combineReducers({
+//   user: userSlice.reducer,
+//   post: postSlice.reducer,
+//   reel: reelSlice.reducer,
+// });
+
+// const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: persistedReducer,
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
+  reducer: {
+    user: userSlice.reducer,
+    post: postSlice.reducer,
+    reel: reelSlice.reducer,
+  },
+  // persistedReducer,
+  // middleware: getDefaultMiddleware =>
+  //   getDefaultMiddleware({
+  //     serializableCheck: {
+  //       ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+  //     },
+  //   }),
 });
