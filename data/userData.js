1811a7281ec9157009326c5fdb015a307ca01ba2
Amlan2002 <iamamlan2002@gmail.com>
2023-08-25 11:28:00 +0530

    post data image change

--- data/userData.js
+++ data/userData.js
@@ -1,56 +1,66 @@
+import AmlanImg from '../assets/images/Amlan.png';
+import ShubhamImg from '../assets/images/Shubham.png';
+import AdityaImg from '../assets/images/Aditya.png';
+import PratikshaImg from '../assets/images/Pratiksha.png';
+import AdyasarImg from '../assets/images/Adyasar.png';
+import GauriImg from '../assets/images/Gauri.png';
+import PrakharImg from '../assets/images/Prakhar.png';
+import RishabhImg from '../assets/images/Rishabh.png';
+import PreetiImg from '../assets/images/Preeti.png';
+
 export const UserData = [
   {
     userId: 1,
     userName: 'Amlanjyoti Sahoo',
-    profileImage: require('../assets/images/Amlan.png'),
+    profileImage: AmlanImg,
     hasStatus: false,
   },
   {
     userId: 2,
     userName: 'Shubham Kadam',
-    profileImage: require('../assets/images/Shubham.png'),
+    profileImage: ShubhamImg,
     hasStatus: false,
   },
   {
     userId: 3,
     userName: 'Aditya Verma',
-    profileImage: require('../assets/images/Aditya.png'),
+    profileImage: AdityaImg,
     hasStatus: true,
   },
   {
     userId: 4,
     userName: 'Pratiksha Maruti',
-    profileImage: require('../assets/images/Pratiksha.png'),
+    profileImage: PratikshaImg,
     hasStatus: false,
   },
   {
     userId: 5,
     userName: 'Adyasar Swagat',
-    profileImage: require('../assets/images/Adyasar.png'),
+    profileImage: AdyasarImg,
     hasStatus: false,
   },
   {
     userId: 6,
     userName: 'Gauri Gajanan',
-    profileImage: require('../assets/images/Gauri.png'),
+    profileImage: GauriImg,
     hasStatus: false,
   },
   {
     userId: 7,
     userName: 'Prakhar Maheshwari',
-    profileImage: require('../assets/images/Prakhar.png'),
+    profileImage: PrakharImg,
     hasStatus: false,
   },
   {
     userId: 8,
     userName: 'Rishabh Deo',
-    profileImage: require('../assets/images/Rishabh.png'),
+    profileImage: RishabhImg,
     hasStatus: false,
   },
   {
     userId: 9,
     userName: 'Preeti Rajendra',
-    profileImage: require('../assets/images/Preeti.png'),
+    profileImage: PreetiImg,
     hasStatus: false,
   },
 ];
